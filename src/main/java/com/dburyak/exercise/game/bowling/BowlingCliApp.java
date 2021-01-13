9ded436bdb8ee516e61d6298bc1a94ca164b47b1
Dmytro Buryak <dmytro.buryak@gmail.com>
2021-01-13 05:14:55 +0200

    Added version info to cli

--- src/main/java/com/dburyak/exercise/game/bowling/BowlingCliApp.java
+++ src/main/java/com/dburyak/exercise/game/bowling/BowlingCliApp.java
@@ -2,10 +2,20 @@ package com.dburyak.exercise.game.bowling;
 
 import picocli.CommandLine;
 import picocli.CommandLine.Command;
+import picocli.CommandLine.IVersionProvider;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Map;
 import java.util.concurrent.Callable;
 
-@Command()
+@Command(
+        name = "bowl-game",
+        description = "Calculates bowling game scores",
+        mixinStandardHelpOptions = true,
+        versionProvider = BowlingCliApp.VersionProvider.class
+)
 public class BowlingCliApp implements Callable<Integer> {
 
     @Override
@@ -16,5 +26,37 @@ public class BowlingCliApp implements Callable<Integer> {
     public static void main(String[] args) {
         var exitCode = new CommandLine(new BowlingCliApp()).execute(args);
         System.exit(exitCode);
+    }
+
+    static class VersionProvider implements IVersionProvider {
+        private static final String versionFileName = "/version.txt";
+        private static final String revisionFileName = "/revision.txt";
+        private static final String buildTimeFileName = "/built_at.txt";
+
+        @Override
+        public String[] getVersion() throws Exception {
+            return Map
+                    .of(
+                            "version  : ", versionFileName,
+                            "revision : ", revisionFileName,
+                            "built_at : ", buildTimeFileName
+                    )
+                    .entrySet().stream()
+                    .map(e -> {
+                        String versionType = e.getKey();
+                        String fileName = e.getValue();
+                        String versionValue;
+                        try (
+                                var inStream = getClass().getResourceAsStream(fileName);
+                                var reader = new BufferedReader(new InputStreamReader(inStream));
+                                ) {
+                            versionValue = reader.readLine();
+                        } catch (IOException ioException) {
+                            versionValue = "unknown";
+                        }
+                        return versionType + " " + versionValue;
+                    })
+                    .toArray(String[]::new);
+        }
     }
 }
